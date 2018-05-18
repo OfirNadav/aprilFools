6a6aeea4b3db427648d1079cf2a7f3da3662c7b0
OfirNadav <ofir@zinkerz.com>
2018-05-18 09:53:52 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'src',
+    livereload: true,
+    port: 3000
+  });
+});
+
+// keeps gulp from crashing for scss errors
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+      .pipe(sass({ errLogToConsole: true }))
+      .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./src/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.scss', ['sass']);
+  gulp.watch('./src/**/*', ['livereload']);
+});
+
+
+
+gulp.task('default', ['connect', 'watch', 'sass']);
